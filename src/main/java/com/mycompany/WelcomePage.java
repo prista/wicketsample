5df136e7c60a12a22c6ef9ff57a1fec56b4f2de0
prista <games.prista@mail.ru>
2019-04-28 19:14:05 +0300

    #vid3 - Ajax Example

--- src/main/java/com/mycompany/WelcomePage.java
+++ src/main/java/com/mycompany/WelcomePage.java
@@ -1,8 +1,9 @@
 package com.mycompany;
 
 import com.mycompany.model.USerModel;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
@@ -13,29 +14,45 @@ import java.util.List;
 
 public class WelcomePage extends WebPage {
 
-    // need to add 'implements Serializable' in USerModel
     private static final long serialVersionUID = 1406354671961504008L;
     private List<String> genderChoices = new ArrayList<>();
 
     public WelcomePage() {
         genderChoices.add("Male");
         genderChoices.add("Female");
-        // когда переменная юзается в inner class то делаем ее final (уточинть)
         final USerModel uSerModel = new USerModel();
 
-        // 1. Let's define all components
         Form<?> form = new Form("form");
 
-        // TextField binding with the "name" field in our USerModel pojo
-        // DropDown binding with the "gender" field in our USerModel pojo
-        // когда мы что-то вводим в инпут, автоматом в Pojo сетается это значение (из за байндинга)
+        // Первый аргумент в конструкторе Label, соответствующий wicket: id, назначенному в файле HTML.
+        // Второй аргумент - это модель компонента, оболочка для базовых данных, представленных в компоненте.
         TextField<String> text = new TextField("text", new PropertyModel(uSerModel, "name"));
+        //The setOutputMarkupId method makes the component eligible for modification via Ajax
+        text.setOutputMarkupId(true);
 
         DropDownChoice<String> gender = new DropDownChoice("gender",
                 new PropertyModel(uSerModel, "gender"), genderChoices);
-        // обычно 3й параметр берется из базы
+        gender.setOutputMarkupId(true);
 
-        Button button = new Button("submit") {
+        AjaxButton ajaxButton = new AjaxButton("submit") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target) {
+                super.onSubmit(target);
+
+                text.setEnabled(false);
+                gender.setEnabled(false);
+
+                // Then AjaxButton is added with the onSubmit callback method
+                // that updates the label’s model and the dropdown's model,
+                // once ajax request is issued (set property enabled false).
+                // Finally, the label and dropdown objects (component)
+                // are set as a target for refreshing.
+                target.add(text);
+                target.add(gender);
+            }
+        };
+
+/*        Button button = new Button("submit") {
             @Override
             public void onSubmit() {
                 super.onSubmit();
@@ -43,17 +60,25 @@ public class WelcomePage extends WebPage {
                 System.out.println("NAME: " + uSerModel.getName());
                 System.out.println("GENDER: " + uSerModel.getGender());
             }
-        };
+        };*/
 
 
-        // 2. We need to add these components in the web page
-
-        // форма у нас сразу в боди лежала,
-        // ее мы должны добавить непосредственно на web page
         add(form);
-        // уже в form мы пихаем ее содержимое
         form.add(text);
         form.add(gender);
-        form.add(button);
+        //form.add(button);
+        form.add(ajaxButton);
     }
 }
+
+// need to add 'implements Serializable' in USerModel
+// 1. Let's define all components
+
+// TextField binding with the "name" field in our USerModel pojo
+// DropDown binding with the "gender" field in our USerModel pojo
+// когда мы что-то вводим в инпут, автоматом в Pojo сетается это значение (из за байндинга)
+
+// 2. We need to add these components in the web page
+// форма у нас сразу в боди лежала,
+// ее мы должны добавить непосредственно на web page
+// уже в form мы пихаем ее содержимое (компоненты другие)
