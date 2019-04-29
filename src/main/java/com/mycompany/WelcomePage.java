d974d5c82c9cff2449c857f105022e9e7a6b3127
prista <games.prista@mail.ru>
2019-04-29 16:07:09 +0300

    #vid5 - How to toggle enable/disable components

--- src/main/java/com/mycompany/WelcomePage.java
+++ src/main/java/com/mycompany/WelcomePage.java
@@ -1,6 +1,11 @@
 package com.mycompany;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
 public class WelcomePage extends WebPage {
 
@@ -8,8 +13,31 @@ public class WelcomePage extends WebPage {
 
     public WelcomePage() {
         // panel is a reusable component that holds markup and other components
-        add(new SamplePanel("panel"));
-        add(new SamplePanel("panel1"));
+/*        add(new SamplePanel("panel"));
+        add(new SamplePanel("panel1"));*/
+
+        Form<?> form = new Form<Object>("form");
+        TextField<String> textField = new TextField<>("textField", new Model<>());
+        textField.setOutputMarkupId(true);
+        // Render a placeholder tag when the component is NOT VISIBLE.
+        // This method will also call setOutputMarkupId(true)
+        textField.setOutputMarkupPlaceholderTag(true);
+
+        form.add(textField);
+
+        form.add(new AjaxButton("submit") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target) {
+                super.onSubmit(target);
+                if (textField.isVisible()) {
+                    textField.setVisible(false);
+                } else {
+                    textField.setVisible(true);
+                }
+                target.add(textField);
+            }
+        });
+        add(form);
     }
 }
 
