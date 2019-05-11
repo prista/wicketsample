bd06f5fd2c2170b7fc810fafece04f70698fc176
prista <games.prista@mail.ru>
2019-05-11 10:15:52 +0300

    #example 1 - Hello World

---
+++ src/main/java/com/tutorial/helloworld/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.tutorial.helloworld;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+public class HelloWorldApplication extends WebApplication {
+    public HelloWorldApplication() {
+    }
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class getHomePage() {
+        return HelloWorld.class;
+    }
+}
